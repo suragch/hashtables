38fa28e1256a97db562544a06e667cd60508422c
suragch <suragch@suragch.dev>
2025-10-07 12:29:46 +0800

    added binary search

--- bin/hashtables.dart
+++ bin/hashtables.dart
@@ -1,7 +1,14 @@
+import 'package:hashtables/binary_search.dart';
 import 'package:hashtables/linear_search.dart';
 
 void main() {
   final myList = [42, 3, 10, 7, 28, 149, 0, 3, 25, 7, 1];
-  final foundIndex = linearSearch(myList, 28);
+  var foundIndex = linearSearch(myList, 28);
+  print(foundIndex);
+
+  myList.sort();
+  print(myList); // [0, 1, 3, 3, 7, 7, 10, 25, 28, 42, 149]
+
+  foundIndex = binarySearch(myList, 1);
   print(foundIndex);
 }
---
+++ lib/binary_search.dart
@@ -0,0 +1,16 @@
+/// `list` must be sorted!
+int? binarySearch(List<int> list, int searchValue) {
+  int low = 0;
+  int high = list.length - 1;
+  while (low <= high) {
+    final middle = (low + high) ~/ 2;
+    if (searchValue == list[middle]) {
+      return middle;
+    } else if (searchValue < list[middle]) {
+      high = middle - 1;
+    } else {
+      low = middle + 1;
+    }
+  }
+  return null;
+}
